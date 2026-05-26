fd16d262708cd002091a787fece50c837f88c79e
Matheus Fellet <matheusmfellet@gmail.com>
2026-05-25 21:48:42 -0300

    feat: Adding get_version function

--- src/models/model_registry.py
+++ src/models/model_registry.py
@@ -4,10 +4,37 @@ import os
 import json
 import torch
 
+from mlflow.tracking import MlflowClient
+
 from src.models.base import ECGClassifier
 from src.models.cnn_bert import CNNBertClassifier
 from src.models.swin_transformer import SwinTransformer
 from src.models.pn_encoder import PreNormEncoderClassifier
+
+
+def get_version(
+    registry_base: str,
+    model_name: str,
+    new_version: bool,
+) -> str:
+    versions = sorted(
+        [d for d in os.listdir(registry_base) if d.startswith("v")],
+        key=lambda v: int(v[1:]),
+    )
+
+    if not versions:
+        if not new_version:
+            raise FileNotFoundError(
+                f"No versions found for model '{model_name}' in '{registry_base}'."
+            )
+
+        model_version = "v1"
+        return model_version
+
+    latest_version = versions[-1]
+    new_version_number = int(latest_version[-1]) + 1
+
+    return f"v{new_version_number}" if new_version else latest_version
 
 
 def instantiate_model_with_params(
@@ -35,17 +62,16 @@ def load_model_from_registry(
         registry_root,
         model_name,
     )
-
     if model_version == "latest":
-        versions = sorted(
-            [d for d in os.listdir(registry_base) if d.startswith("v")],
-            key=lambda v: int(v[1:]),
+        model_version = get_version(
+            registry_base,
+            new_version=False,
+            model_name=model_name,
         )
-        if not versions:
-            raise FileNotFoundError(
-                f"No versions found for model '{model_name}' in '{registry_base}'."
-            )
-        model_version = versions[-1]
+    elif model_version == "production":
+        client = MlflowClient()
+        alias_info = client.get_model_version_by_alias(model_name, "production")
+        model_version = f"v{alias_info.version}"
 
     model_registry_path = os.path.join(
         registry_base,
